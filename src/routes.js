b5b319d734c1a120f119602641fb25a00dc9b43d
JulianSanchez69 <juliansanc@unicauca.edu.co>
2023-07-21 17:38:55 -0500

    Subiendo verificación de usuarios

--- src/routes.js
+++ src/routes.js
@@ -11,4 +11,30 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'An error occurred while fetching the user' });
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (user) {
+      await user.update(req.body);
+      res.json(user);
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+  } catch (err) {
+    res.status(400).json({ error: 'An error occurred while updating the user' });
+  }
+});
 module.exports = router;
